240dca8df5636df9838ad1d95a86fb0f9fb2ba9e
Jhyzel Orcega <jdorcega@iskolarngbayan.pup.edu.ph>
2021-04-19 10:32:58 +0800

    Add project files.

---
+++ EmployeesTimeKeepingSystem/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EmployeesTimeKeepingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee Number:"); 
+            string num = Console.ReadLine();
+          
+
+            Console.WriteLine("Enter Last Name:");
+            string last = Console.ReadLine();
+            
+
+            Console.WriteLine("Enter First Name:");
+            string first = Console.ReadLine();
+          
+
+            Console.WriteLine("Enter Department:");
+            string depart = Console.ReadLine();
+         //   Console.ReadKey();
+
+      
+
+            Console.WriteLine("Employee Number: " + num);
+           
+
+            Console.WriteLine("Employee Number: " + last);
+       
+
+            Console.WriteLine("Employee Number: " + first);
+            
+
+            Console.WriteLine("Employee Number: " + depart);
+            Console.ReadKey();
+
+
+        }
+    }
+}
