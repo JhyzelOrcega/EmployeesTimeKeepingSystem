5666e2655b11a1380fbd3090d42cb8e536e8dac2
JhyzelOrcega <81238655+JhyzelOrcega@users.noreply.github.com>
2021-08-30 20:25:25 +0800

    Update UserSearch.cs

--- EmployeesTimeKeepingSystem/UserSearch.cs
+++ EmployeesTimeKeepingSystem/UserSearch.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace EmployeesTimeKeepingSystem
 {
     class UserSearch
     {
+        throw new NotImplementedException();
     }
 }
